4f3c9a9de587ef61485183d93322d1c18e00c44c
Kelsie Merchant <eislek02@gmail.com>
2020-12-02 19:51:07 -0600

    Updated to combine separate function into one function for the checker to be able to import the function

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,21 +4,23 @@
 
 def add_matrices2D(mat1, mat2):
     """ returns new matrix, the sum of two 2D matrices added element-wise """
-    if matrix_shape(mat1) != matrix_shape(mat2):
+    mat1_shape = []
+    matrix = mat1
+    while (type(matrix) is list):
+        mat1_shape.append(len(matrix))
+        matrix = matrix[0]
+    mat2_shape = []
+    matrix = mat2
+    while (type(matrix) is list):
+        mat2_shape.append(len(matrix))
+        matrix = matrix[0]
+
+    if mat1_shape != mat2_shape:
         return None
     sum_matrix = []
-    for index, dimension in enumerate(matrix_shape(mat1)):
+    for index, dimension in enumerate(mat1_shape):
         for idx, i in enumerate(range(dimension)):
             if idx is 0:
                 sum_matrix.append([])
             sum_matrix[index].append(mat1[index][i] + mat2[index][i])
     return sum_matrix
-
-
-def matrix_shape(matrix):
-    """ returns list of integers representing dimensions of given matrix """
-    matrix_shape = []
-    while (type(matrix) is list):
-        matrix_shape.append(len(matrix))
-        matrix = matrix[0]
-    return matrix_shape
