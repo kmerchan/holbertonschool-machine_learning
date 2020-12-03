0261fbd6a0b66573d4feb1c0f4cb31dc7a7d00e2
Kelsie Merchant <eislek02@gmail.com>
2020-12-03 01:23:48 -0600

    Add function that performs matrix multiplication for two matrices as numpy.ndarrays; update test to remove unnecessary line

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" defines function that performs matrix multiplication with numpy """
+
+
+def np_matmul(mat1, mat2):
+    """ performs matrix multiplication using numpy.ndarrays """
+    return (mat1 @ mat2)
