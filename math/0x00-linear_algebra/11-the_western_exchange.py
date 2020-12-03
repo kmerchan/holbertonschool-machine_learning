694910bea4347ce9f13f39676cae73bd0214af35
Kelsie Merchant <eislek02@gmail.com>
2020-12-03 01:46:23 -0600

    Add function that transposes a matrix using numpy.ndarray; update test to remove unnecessary line

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" defines function that transposes a matrix using numpy """
+
+
+def np_transpose(matrix):
+    """ returns new numpy.ndarray that is a transpose of a matrix """
+    return matrix.transpose()
--- math/0x00-linear_algebra/test_files/11-main.py
+++ math/0x00-linear_algebra/test_files/11-main.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 11-main.py
 #!/usr/bin/env python3
 
 import numpy as np
