839d52d0d8d98f5e4ce42a1e4c23bbeb2208fa9f
Kelsie Merchant <eislek02@gmail.com>
2020-12-04 13:54:27 -0600

    Add function that add two matrices and add test file for new function

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" defines function that adds two matrices """
+
+
+def matrix_shape(matrix):
+    """ returns list of integers representing dimensions of given matrix """
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
+
+
+def add_matrices(mat1, mat2):
+    """ returns new matrix that is sum of two matrices added element-wise """
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    if len(matrix_shape(mat1)) is 1:
+            return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
