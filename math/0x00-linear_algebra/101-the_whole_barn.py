28f68a5ae5dc94912d8b8c13b9257b0cf2402393
Kelsie Merchant <eislek02@gmail.com>
2020-12-04 15:03:14 -0600

    Fix Python styling errors

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -4,8 +4,9 @@
 
 def matrix_shape(matrix):
     """ returns list of integers representing dimensions of given matrix """
+
     matrix_shape = []
-    while (type(matrix) is list):
+    while type(matrix) is list:
         matrix_shape.append(len(matrix))
         matrix = matrix[0]
     return matrix_shape
@@ -13,8 +14,9 @@ def matrix_shape(matrix):
 
 def add_matrices(mat1, mat2):
     """ returns new matrix that is sum of two matrices added element-wise """
+
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
     if len(matrix_shape(mat1)) is 1:
-            return [mat1[i] + mat2[i] for i in range(len(mat1))]
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
     return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
