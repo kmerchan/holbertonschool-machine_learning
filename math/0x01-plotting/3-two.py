be0f8d6834f88b8bb81cf295b5f63724d8f8356f
Kelsie Merchant <eislek02@gmail.com>
2020-12-07 15:28:46 -0600

    Update code to fix style error

--- math/0x01-plotting/3-two.py
+++ math/0x01-plotting/3-two.py
@@ -10,7 +10,7 @@ t2 = 1600
 y1 = np.exp((r / t1) * x)
 y2 = np.exp((r / t2) * x)
 
-plt.plot(x, y1,'r--', label='C-14')
+plt.plot(x, y1, 'r--', label='C-14')
 plt.plot(x, y2, 'g-', label='Ra-226')
 plt.xlabel('Time (years)')
 plt.ylabel('Fraction Remaining')
