5d5b22d6f40b3c5f83e72e9129f9eed7d0f55178
Kelsie Merchant <eislek02@gmail.com>
2020-12-07 11:52:51 -0600

    Add completed source code that plots y as a solid red line graph with x-axis from 0-10

--- math/0x01-plotting/0-line.py
+++ math/0x01-plotting/0-line.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" plots y as a line graph """
 import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-# your code here
+plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y, 'r-')
+plt.show()
