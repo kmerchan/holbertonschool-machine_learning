9295de2c5af03815cb0af4cc9e214670893c10d1
Kelsie Merchant <eislek02@gmail.com>
2020-12-07 18:11:55 -0600

    Add completed source code that combines all 5 previous plots into one figure in a 3x2 grid with the last row spanning two columns width

--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" plots all 5 previous graphs in one figure """
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -25,4 +26,47 @@ y32 = np.exp((r3 / t32) * x3)
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-# your code here
+fig = plt.figure()
+
+ax1=fig.add_subplot(3, 2, 1)
+ax1.plot(y0, 'r-')
+ax1.set_xlim((0, 10))
+
+ax2=fig.add_subplot(3, 2, 2)
+ax2.scatter(x1, y1, c='m')
+ax2.set_xlabel('Height (in)', fontsize='x-small')
+ax2.set_ylabel('Weight (lbs)', fontsize='x-small')
+ax2.set_title("Men's Height vs Weight", fontsize='x-small')
+
+ax3=fig.add_subplot(3, 2, 3)
+ax3.plot(x2, y2)
+ax3.set_xlabel('Time (years)', fontsize='x-small')
+ax3.set_ylabel('Fraction Remaining', fontsize='x-small')
+ax3.set_title("Exponential Decay of C-14", fontsize='x-small')
+ax3.set_yscale("log")
+ax3.set_xlim((0, 28650))
+
+ax4=fig.add_subplot(3, 2, 4)
+ax4.plot(x3, y31, 'r--', label='C-14')
+ax4.plot(x3, y32, 'g-', label='Ra-226')
+ax4.set_xlabel('Time (years)', fontsize='x-small')
+ax4.set_ylabel('Fraction Remaining', fontsize='x-small')
+ax4.set_title("Exponential Decay of Radioactive Elements", fontsize='x-small')
+ax4.legend()
+ax4.set_xlim((0, 20000))
+ax4.set_ylim((0, 1))
+
+ax5=fig.add_subplot(3, 1, 3)
+ax5.hist(student_grades,
+         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+         edgecolor='black')
+ax5.set_xlabel('Grades', fontsize='x-small')
+ax5.set_ylabel('Number of Students', fontsize='x-small')
+ax5.set_title("Project A", fontsize='x-small')
+ax5.set_xlim((0, 100))
+ax5.set_xticks(np.arange(0, 101, 10))
+ax5.set_ylim((0, 30))
+
+fig.suptitle("All in One")
+plt.tight_layout()
+plt.show()
