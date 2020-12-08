0023f021c2796269123f7d63db28330c43df128e
Kelsie Merchant <eislek02@gmail.com>
2020-12-07 22:06:35 -0600

    Add completed source code that plots a 3D scatter plot for PCA (Principle Component Analysis) for Iris Dataset; add original source code and pca.npz data source file to source_code directory

---
+++ math/0x01-plotting/101-pca.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" plot the given PCA data as a 3D scatter plot """
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+lib = np.load("pca.npz")
+data = lib["data"]
+labels = lib["labels"]
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+pca_data = np.matmul(norm_data, Vh[:3].T)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2],
+           c=labels, cmap=plt.get_cmap('plasma'), label=labels)
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+plt.title("PCA of Iris Dataset")
+plt.show()
