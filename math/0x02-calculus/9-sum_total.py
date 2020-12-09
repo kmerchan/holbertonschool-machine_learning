7cd49e94609a55d0c3128a7b48d8739e1ac77035
Kelsie Merchant <eislek02@gmail.com>
2020-12-09 12:27:51 -0600

    Refeactor code to call function recursively directly, removes helper function

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,12 +6,8 @@ def summation_i_squared(n):
     """ calculates summation of i^2 from i=1 to n """
     if type(n) is not int:
         return None
-    return sigma_recursion(n, 0)
-
-
-def sigma_recursion(n, sigma_sum):
-    """ calculate summation recursively """
     if n is 1:
-        return (sigma_sum + 1)
-    sigma_sum += n**2
-    return sigma_recursion(n - 1, sigma_sum)
+        return (1)
+    sigma_sum = n**2
+    sigma_sum += summation_i_squared(n - 1)
+    return sigma_sum
