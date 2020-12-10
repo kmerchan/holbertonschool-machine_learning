455bb08b67e7d00702b69b9b1a5ef6a7d27cf893
Kelsie Merchant <eislek02@gmail.com>
2020-12-09 20:01:08 -0600

    Add additional test cases if given 0 polynomial

--- math/0x02-calculus/test_files/17-main.py
+++ math/0x02-calculus/test_files/17-main.py
@@ -18,3 +18,7 @@ poly = [5, 3, 0, 1]
 print(poly_integral(poly, 4.5))
 poly = [5, 3, 0, 1]
 print(poly_integral(poly, 4.00))
+poly = [0, 0, 0, 0]
+print(poly_integral(poly))
+poly = [0, 0, 0, 0]
+print(poly_integral(poly, 4))
