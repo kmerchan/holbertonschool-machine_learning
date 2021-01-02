b6eb39c6cb3bdfb808dad1579cec6df049add384
Kelsie Merchant <eislek02@gmail.com>
2021-01-02 16:46:29 -0600

    Try adding conditions back and removing try/except block

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -21,12 +21,9 @@ def one_hot_encode(Y, classes):
         one-hot encoding of Y with shape (classes, m)
             or None if fails
     """
-    if type(Y) is not np.ndarray:
+    if type(Y) is not np.ndarray or len(Y.shape) != 1 or len(Y) < 1:
         return None
-    if type(classes) is not int or classes != (np.amax(Y) + 1):
+    if type(classes) is not int:
         return None
-    try:
-        one_hot = np.eye(classes)[Y].transpose()
-        return one_hot
-    except Exception:
-        return None
+    one_hot = np.eye(classes)[Y].transpose()
+    return one_hot
