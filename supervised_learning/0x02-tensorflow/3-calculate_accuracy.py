69ca619ac6b966d9aa55fbaff0c4db9204855f16
Kelsie Merchant <eislek02@gmail.com>
2021-01-08 03:44:53 +0000

    Add function that calculates accuracy of a prediction for a neural network using TensorFlow; add test file

---
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates the accuracy of a prediction
+for the neural network
+"""
+
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Calculates the accuracy of a prediction for the neural network
+
+    parameters:
+        y [tf.placeholder]: placeholder for labels of the input data
+        y_pred [tensor]: contains network's predictions
+
+    returns:
+        tensor containing decimal accuracy of the prediction
+    """
+    equality = tf.math.equal(y_pred, y)
+    accuracy = tf.reduce_mean(tf.cast(equality, "float"))
+    return accuracy
