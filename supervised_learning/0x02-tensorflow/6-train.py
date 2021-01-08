67449ff5baaa345dc7fdcb38383989df01e075e7
Kelsie Merchant <eislek02@gmail.com>
2021-01-08 05:43:09 +0000

    Recalculate loss and accuracy for both training and validation before printing last iteration

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -64,9 +64,17 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
                 print("\tValidation Cost: {}".format(loss_valid))
                 print("\tValidation Accuracy: {}".format(accuracy_valid))
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        loss_train = sess.run(loss,
+                              feed_dict={x: X_train, y: Y_train})
+        accuracy_train = sess.run(accuracy,
+                                  feed_dict={x: X_train, y: Y_train})
+        loss_valid = sess.run(loss,
+                              feed_dict={x: X_valid, y: Y_valid})
+        accuracy_valid = sess.run(accuracy,
+                                  feed_dict={x: X_valid, y: Y_valid})
         print("After {} iterations:".format(i))
         print("\tTraining Cost: {}".format(loss_train))
         print("\tTraining Accuracy: {}".format(accuracy_train))
         print("\tValidation Cost: {}".format(loss_valid))
         print("\tValidation Accuracy: {}".format(accuracy_valid))
-        return (saver.save(sess, save_path))
+        return saver.save(sess, save_path)
