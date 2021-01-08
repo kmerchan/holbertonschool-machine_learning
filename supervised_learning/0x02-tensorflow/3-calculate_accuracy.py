db027d6b4209e5d15b26a96ae0ec7b4629cd3f3b
Kelsie Merchant <eislek02@gmail.com>
2021-01-08 03:50:12 +0000

    Update function to call argmax for both the prediction and labels before getting equality

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -19,6 +19,8 @@ def calculate_accuracy(y, y_pred):
     returns:
         tensor containing decimal accuracy of the prediction
     """
+    y_pred = tf.math.argmax(y_pred, axis=1)
+    y = tf.math.argmax(y, axis=1)
     equality = tf.math.equal(y_pred, y)
     accuracy = tf.reduce_mean(tf.cast(equality, "float"))
     return accuracy
