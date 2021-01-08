85cdc27c10e7ab8ff72d711d7412c99c8a96e9dd
Kelsie Merchant <eislek02@gmail.com>
2021-01-07 23:36:27 -0600

    Move imports for Python methods to beginning of file

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -6,6 +6,11 @@ neural network classifier
 
 
 import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
@@ -27,13 +32,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     returns:
         path to where model was saved
     """
-    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-    calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = __import__(
-        '0-create_placeholders').create_placeholders
-    create_train_op = __import__('5-create_train_op').create_train_op
-    forward_prop = __import__('2-forward_prop').forward_prop
-
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
