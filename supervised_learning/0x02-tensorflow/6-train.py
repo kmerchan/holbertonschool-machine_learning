9ebe57cc8c496ba32e7c90479d3f9fb2dfcb2127
Kelsie Merchant <eislek02@gmail.com>
2021-01-07 23:32:51 -0600

    Adjust long lines and removing trailing whitespace to follow pycodestyle guidlines

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -29,7 +29,8 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     """
     calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
     calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = __import__('0-create_placeholders').create_placeholders
+    create_placeholders = __import__(
+        '0-create_placeholders').create_placeholders
     create_train_op = __import__('5-create_train_op').create_train_op
     forward_prop = __import__('2-forward_prop').forward_prop
 
@@ -44,16 +45,20 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     tf.add_to_collection('loss', loss)
     train_op = create_train_op(loss, alpha)
     tf.add_to_collection('train_op', train_op)
-    
+
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for i in range(iterations):
-            loss_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            accuracy_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            loss_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-            accuracy_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+            loss_train = sess.run(loss,
+                                  feed_dict={x: X_train, y: Y_train})
+            accuracy_train = sess.run(accuracy,
+                                      feed_dict={x: X_train, y: Y_train})
+            loss_valid = sess.run(loss,
+                                  feed_dict={x: X_valid, y: Y_valid})
+            accuracy_valid = sess.run(accuracy,
+                                      feed_dict={x: X_valid, y: Y_valid})
             if (i % 100) is 0:
                 print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(loss_train))
