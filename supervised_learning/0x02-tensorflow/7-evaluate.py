3f7c612effc4ac3915cc2c7f37b2888fdad8b142
Kelsie Merchant <eislek02@gmail.com>
2021-01-08 05:51:03 +0000

    Initial commit

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Defines a function that evaluates output of
+neural network classifier
+"""
+
+
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Evaluates output of neural network
+
+    parameters:
+        X [numpy.ndarray]: contains the input data to evaluate
+        Y [numpy.ndarray]: contains the one-hot labels for X
+        save_path [string]: location to load the model from
+
+    returns:
+        the network's prediction, accuracy, and loss, respectively
+    """
+    pass
