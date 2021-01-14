129fcdf845a3571e31522ebb3c7d44de7e4f1691
Kelsie Merchant <eislek02@gmail.com>
2021-01-14 20:17:13 +0000

    Fix check for if total data points are divisible by batch_size and use new variable to hold shuffled data separate from X_train, Y_tain

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -80,30 +80,31 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
             print("\tValidation Accuracy: {}".format(valid_accuracy))
             if epoch == epochs:
                 break
-            X_train, Y_train = shuffle_data(X_train, Y_train)
-            if (m % batch_size):
+            X_train_s, Y_train_s = shuffle_data(X_train, Y_train)
+            if (m % batch_size) is 0:
                 mini_batch_total = m // batch_size
             else:
                 mini_batch_total = (m // batch_size) + 1
+
             step_number = 0
             for mini_batch in range(mini_batch_total):
                 low = mini_batch * batch_size
                 high = ((mini_batch + 1) * batch_size)
                 if high > m:
                     high = m
-                sess.run(train_op, feed_dict={x: X_train[low:high, :],
-                                              y: Y_train[low:high, :]})
+                sess.run(train_op, feed_dict={x: X_train_s[low:high, :],
+                                              y: Y_train_s[low:high, :]})
                 step_number += 1
                 if (step_number % 100) is 0:
                     print("\tStep {}:".format(step_number))
                     step_cost = sess.run(
                         loss,
-                        feed_dict={x: X_train[low:high, :],
-                                   y: Y_train[low:high, :]})
+                        feed_dict={x: X_train_s[low:high, :],
+                                   y: Y_train_s[low:high, :]})
                     print("\t\tCost: {}".format(step_cost))
                     step_accuracy = sess.run(
                         accuracy,
-                        feed_dict={x: X_train[low:high, :],
-                                   y: Y_train[low:high, :]})
+                        feed_dict={x: X_train_s[low:high, :],
+                                   y: Y_train_s[low:high, :]})
                     print("\t\tAccuracy: {}".format(step_accuracy))
         return (saver.save(sess, save_path))
