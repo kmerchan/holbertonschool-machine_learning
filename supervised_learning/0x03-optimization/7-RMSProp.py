2da9630d62ace7809c676b8be1ced1fb53321aab
Kelsie Merchant <eislek02@gmail.com>
2021-01-14 23:54:17 -0600

    Add function that updates a variable using RMSProp optimization algorithm

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -21,6 +21,12 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
         s [tf.moment]:
             the previous second moment of var
 
+    s_dW = (beta * s_dW) + ((1 - beta) * (dW ** 2))
+    W = W - (alpha * (dW / sqrt(s_dW)))
+
     returns:
         the updated variable and the new moment, respectively
     """
+    s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    var -= alpha * (grad / (epsilon + (s_dW ** (1 / 2))))
+    return var, s_dW
