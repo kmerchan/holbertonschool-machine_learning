827bf2915f134eb7acf64fb2690212d2ed5dca64
Kelsie Merchant <eislek02@gmail.com>
2021-01-15 00:04:20 -0600

    Add function that updates a variable in place using the Adam optimization algorithm

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -24,13 +24,16 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
             the previous second moment of var
         t [int]: time step used for bias correction
 
+    v_dW = (beta * v_dW) + ((1 - beta) * dW)
+    s_dW = (beta * s_dW) + ((1 - beta) * (dW ** 2))
+
     returns:
         the updated variable, the new first moment, and the new second moment,
             respectively
     """
     v_dW = (beta1 * v) + ((1 - beta1) * grad)
     s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
-    v_dW /= (1 - (beta1 ** t))
-    s_dW /= (1 - (beta2 ** t))
-    var -= alpha * (v_dW / (epsilon + (s_dW ** (1 / 2))))
+    v_dW_c = v_dW / (1 - (beta1 ** t))
+    s_dW_c = s_dW / (1 - (beta2 ** t))
+    var -= alpha * (v_dW_c / (epsilon + (s_dW_c ** (1 / 2))))
     return var, v_dW, s_dW
