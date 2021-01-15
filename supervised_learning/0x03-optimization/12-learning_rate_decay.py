14ee93f54cd466d6aa65b037aedffaa7ec301920
Kelsie Merchant <eislek02@gmail.com>
2021-01-14 22:54:18 -0600

    Add function that creates a learning rate decay operation in TensorFlow using inverse time decay

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Defines function that creates a learning rate decay op
+for a neural network in TensorFlow using inverse time decay
+"""
+
+
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Creates a learning rate decay op for a neural network in TensorFlow
+        using inverse time decay
+
+    parameters:
+        alpha [float]: original learning rate
+        decay_rate: wight used to determine the rate at which alpha will decay
+        global_step [int]:
+            number of passes of gradient descent that have elapsed
+        decay_step [int]:
+            number of passes of gradient descent that should occur before
+                alpha is decayed furtherXS
+
+    the learning rate decay should occur in a stepwise fashion
+
+    returns:
+        the learning rate decay operation
+    """
+    op = tf.train.inverse_time_decay(
+        alpha, global_step, decay_step, decay_rate)
+    return op
