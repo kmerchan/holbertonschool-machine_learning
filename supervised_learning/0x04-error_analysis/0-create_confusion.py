fccaef547b4bb82e843470682dc2b99b9ede935e
Kelsie Merchant <eislek02@gmail.com>
2021-01-20 21:21:38 -0600

    Add function that creates a confusion matrix based on given labels and prediction

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Defines function that creates a confusion matrix
+from given labels and predictions
+"""
+
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    Creates a confusion matrix from given labels and prediction
+
+    parameters:
+        labels [numpy.ndarray of shape (m, classes)]:
+            contains the correct labels of each data point
+            m: number of data points
+            classes: number of classes
+        logits [numpy.ndarray of shape (m, classes)]:
+            contains the predicted labels of each data point
+            m: number of data points
+            classes: number of classes
+
+    returns:
+        confusion matrix [numpy.ndarray of shape (classes, classes)
+            with row indices representing correct labels
+            and column indices representing predicted labels
+    """
+    return np.matmul(labels.transpose(), logits)
