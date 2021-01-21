b50fed8acbd683deb61266a209d9f92e1f316584
Kelsie Merchant <eislek02@gmail.com>
2021-01-20 23:22:54 -0600

    Add re-typed file defining function to calculate confusion matrix

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 """
-Defines function that creates a confusion matrix
-from given labels and predictions
+Defines a function that creates a confusion matrix
 """
+
 import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
     """
-    Creates a confusion matrix from given labels and prediction
+    Creates a confusion matrix
 
     parameters:
         labels [numpy.ndarray of shape (m, classes)]:
-            contains the correct labels of each data point
+            contains correct labels for each data point
             m: number of data points
             classes: number of classes
         logits [numpy.ndarray of shape (m, classes)]:
-            contains the predicted labels of each data point
+            contains predicted labels for each data point
             m: number of data points
             classes: number of classes
 
     returns:
-        confusion matrix [numpy.ndarray of shape (classes, classes)
-            with row indices representing correct labels
-            and column indices representing predicted labels
+        confusion matrix [numpy.ndarray of shape (classes, classes)]:
+            row indices represent correct labels
+            column indices represent predicted labels
     """
     return np.matmul(labels.transpose(), logits)
