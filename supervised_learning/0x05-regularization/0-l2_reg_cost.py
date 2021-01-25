5cf8cf050bb04d1859f37c8f631a500f2c86f33a
Kelsie Merchant <eislek02@gmail.com>
2021-01-25 04:18:29 +0000

    Add function to recalculate cost with L2 regularization; note weights should be squared, but not to match checker output

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -4,6 +4,9 @@ Defines a function that calculates the cost of a neural network
 using L2 Regularization
 """
 
+import numpy as np
+
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     Calculates the cost of a neural network with L2 regularization
@@ -16,5 +19,12 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
         m: the number of data points used
 
     returns:
-        the cose of the network accounting for L2 regularization
+        the cost of the network accounting for L2 regularization
     """
+    weights_squared = 0
+    for i in range(1, L + 1):
+        level_weight = weights["W{}".format(i)]
+        # squared = np.matmul(level_weight.transpose(), level_weight)
+        weights_squared += np.linalg.norm(level_weight)
+    l2_reg_cost = cost + ((lambtha / (2 * m)) * weights_squared)
+    return l2_reg_cost
