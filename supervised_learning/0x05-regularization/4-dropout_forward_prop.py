467ac60d1bdbbc53c84aeaee7db8ae1dca3cf60b
Kelsie Merchant <eislek02@gmail.com>
2021-01-24 23:57:26 -0600

    Initial commit

---
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Initial placeholder
+"""
+
+import tensorflow as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """
+    Initial placeholder
+    """
+    pass
