be058e204ed7d1ab5dffc7deedc7145742a95df4
Kelsie Merchant <eislek02@gmail.com>
2021-01-31 11:43:02 -0600

    Update ranges to height and width since padding adjusts output to be the same size as images

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -39,8 +39,8 @@ def convolve_grayscale_same(images, kernel):
     images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
                     'constant', constant_values=0)
     convoluted = np.zeros((m, height, width))
-    for h in range(height - kh + 1):
-        for w in range(width - kw + 1):
+    for h in range(height):
+        for w in range(width):
             output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
                             axis=1).sum(axis=1)
             convoluted[:, h, w] = output
