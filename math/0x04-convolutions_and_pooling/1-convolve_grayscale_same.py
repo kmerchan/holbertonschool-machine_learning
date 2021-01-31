ca26cc0191689ea5201eec670964f93c2627345a
Kelsie Merchant <eislek02@gmail.com>
2021-01-31 11:35:31 -0600

    Add function that performs same convolution on grayscale images using only two for loops

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Defines a function that performs same convolution
+on a grayscale image
+"""
+
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """
+    Performs a same convolution on grayscale images
+
+    parameters:
+        images [numpy.ndarray with shape (m, h, w)]:
+            contains multiple grayscale images
+            m: number of images
+            h: height in pixels of all images
+            w: width in pixels of all images
+        kernel [numpy.ndarray with shape (kh, kw)]:
+            contains the kernel for the convolution
+            kh: height of the kernel
+            kw: width of the kernel
+
+    if needed, images should be padded with 0s
+    function may only use two for loops maximum and no other loops are allowed
+
+    returns:
+        numpy.ndarray contained convolved images
+    """
+    m = images.shape[0]
+    height = images.shape[1]
+    width = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    ph = (kh - 1) // 2
+    pw = (kw - 1) // 2
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                    'constant', constant_values=0)
+    convoluted = np.zeros((m, height, width))
+    for h in range(height - kh + 1):
+        for w in range(width - kw + 1):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
+                            axis=1).sum(axis=1)
+            convoluted[:, h, w] = output
+    return convoluted
