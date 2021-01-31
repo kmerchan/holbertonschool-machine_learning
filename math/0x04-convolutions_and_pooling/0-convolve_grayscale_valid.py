3f967a42358926aa74be8c196a46e97d6bec8a6f
Kelsie Merchant <eislek02@gmail.com>
2021-01-31 01:04:14 -0600

    Update to utilize np.matmul()

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -36,8 +36,8 @@ def convolve_grayscale_valid(images, kernel):
     convoluted = np.zeros((m, height - kh + 1, width - kw + 1))
     for h in range(height - kh):
         for w in range(width - kw):
-            output = np.sum(
-                images[:, h: h + kh, w: w + kw] * kernel,
-                axis=1).sum(axis=1)
+            output = np.sum(np.multiply(
+                images[:, h: h + kh, w: w + kw], kernel),
+                            axis=1).sum(axis=1)
             convoluted[:, h, w] = output
     return convoluted
