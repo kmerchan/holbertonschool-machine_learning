22930e77c9ba8728f616450676e8ad78bff5410c
Kelsie Merchant <eislek02@gmail.com>
2021-01-31 11:24:36 -0600

    Add 1 to range to include farthest right or bottom square of image that the kernal can be applied to

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -34,8 +34,8 @@ def convolve_grayscale_valid(images, kernel):
     kh = kernel.shape[0]
     kw = kernel.shape[1]
     convoluted = np.zeros((m, height - kh + 1, width - kw + 1))
-    for h in range(height - kh):
-        for w in range(width - kw):
+    for h in range(height - kh + 1):
+        for w in range(width - kw + 1):
             output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
                             axis=1).sum(axis=1)
             convoluted[:, h, w] = output
