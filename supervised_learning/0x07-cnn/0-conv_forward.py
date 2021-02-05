Update calculation of padding if padding is 'same' to fix algebra error; should not include the +1 as it is factored into (h_prev - 1) or (w_prev - 1) term

supervised_learning/0x07-cnn/0-conv_forward.py
@@ -46,8 +46,8 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         ph = 0
         pw = 0
     elif padding is 'same':
-        ph = ((((h_prev - 1) * sh) + kh - h_prev) // 2) + 1
-        pw = ((((w_prev - 1) * sw) + kw - w_prev) // 2) + 1
+        ph = ((((h_prev - 1) * sh) + kh - h_prev) // 2)
+        pw = ((((w_prev - 1) * sw) + kw - w_prev) // 2)
     else:
         return
     images = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),