dad284385415e1559b5485bdf96a830e8239b62a
Kelsie Merchant <eislek02@gmail.com>
2021-02-14 14:16:56 -0600

    Add function that builds a dense block using bottleneck layers for DenseNet-B

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -31,4 +31,19 @@ def dense_block(X, nb_filters, growth_rate, layers):
         the concatenated output of each layer within the dense block
             and the number of filter within the concatenated outputs
     """
+    for layer in layers:
+        Batch_Norm1 = K.layers.BatchNormalization(axis=3)(X)
+        ReLU1 = K.layers.Activation(activation)(Batch_Norm1)
+        C1 = K.layers.Conv2D(filters=(4 * growth_rate),
+                             kernel_size=(1, 1),
+                             padding='same',
+                             kernel_initializer=init)(ReLU1)
+        Batch_Norm3 = K.layers.BatchNormalization(axis=3)(C1)
+        ReLU3 = K.layers.Activation(activation)(Batch_Norm3)
+        C3 = K.layers.Conv2D(filters=growth_rate,
+                             kernel_size=(3, 3),
+                             padding='same',
+                             kernel_initializer=init)(ReLU3)
+        X = K.layers.concatenate([X, C3])
+        nb_filters += growth_rate
     return X, nb_filters
