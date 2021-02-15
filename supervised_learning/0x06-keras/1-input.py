bcbe5d5d1da7fbd948e4eb0865caa8986a55128d
Kelsie Merchant <eislek02@gmail.com>
2021-02-15 10:03:23 -0600

    Add function that builds neural network using Keras library with Inputs

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Defines a function that builds a neural network
+using Keras library
+"""
+
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network using Keras library
+
+    parameters:
+        nx [int]: number of input features to the network
+        layers [list]:
+            contains the number of nodes in each layer of the network
+        activations [list]:
+            contains the activation functions used for each layer
+        lambtha [float]:
+            the L2 regularization parameter
+        keep_prob [float]:
+            the probability that a node will be kept for dropout
+
+    returns:
+        the keras model
+    """
+    reg_l2 = K.regularizers.l2(lambtha)
+    inputs = K.Input(shape=(nx,))
+    X = inputs
+    for i in range(len(layers)):
+        if i is not 0:
+            X = K.layers.Dropout(1 - keep_prob)(X)
+        X = K.layers.Dense(layers[i],
+                           activation=activations[i],
+                           kernel_regularizer=reg_l2)(X)
+    model = K.Model(inputs=inputs, outputs=X)
+    return model
