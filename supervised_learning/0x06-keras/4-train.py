c8e6f7a4b65f09aff16cec77ce68ab91aaf5b444
Kelsie Merchant <eislek02@gmail.com>
2021-02-15 17:47:16 +0000

    Initial commit of function to train model with mini-batch gradient descent

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Defines a function that trains a model using mini-batch gradient descent
+using Keras library
+"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent with Keras
+
+    parameters:
+        network [keras model]: model to train
+        data [numpy.ndarray of shape (m, nx)]:
+            contains the input data
+        labels [one-hot numpy.ndarray of shape (m, classes)]:
+            contains labels of data
+        batch_size [int]:
+            size of batch used for mini-batch gradient descent
+        epochs [int]:
+            number of passes through data for mini-batch gradient descent
+        verbose [boolean]:
+            determines if output should be printed during training
+        shuffle [boolean]:
+            determines whether to shuffle the batches every epoch
+
+    returns:
+        the History object generated after training the model
+    """
+    
