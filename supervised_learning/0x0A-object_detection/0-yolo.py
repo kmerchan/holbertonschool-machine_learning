8a95dde5938696c236c19a8af5250c409b2d5602
Kelsie Merchant <eislek02@gmail.com>
2021-02-24 19:11:27 +0000

    Update class constructor to save class_names as a list, fix Keras method module to get load_model(); fix errors found from testing in sandbox

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -40,9 +40,12 @@ class Yolo:
                 anchor_boxes: number of anchor boxes used for each prediction
                 2: [anchor_box_width, anchor_box_height]
         """
-        self.model = K.load_model(model_path)
+        self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
-            self.class_names = f.read()
+            lines = f.readlines()
+            self.class_names = []
+            for name in lines:
+                self.class_names.append(name[:-1])
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
