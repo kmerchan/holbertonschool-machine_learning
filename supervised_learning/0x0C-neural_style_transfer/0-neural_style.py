ebb0e997f2cbeb5d2989c2c3d106967de6103a7d
Kelsie Merchant <eislek02@gmail.com>
2021-02-24 21:18:29 -0600

    Initial commit

---
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defines class NST that performs tasks for neural style transfer
+"""
+
+
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """
+    Performs tasks for Neural Style Transfer
+    """
