98b8d6620b1d75fe968f05cdd13fdb638f89042e
Kelsie Merchant <eislek02@gmail.com>
2021-03-04 15:13:28 -0600

    Add method to filter boxes if above given class threshold in Yolo class based on notes from global live coding

--- supervised_learning/0x0A-object_detection/2-yolo.py
+++ supervised_learning/0x0A-object_detection/2-yolo.py
@@ -99,7 +99,41 @@ class Yolo:
                     (grid_height, grid_width, anchor_boxes, classes)]:
                     contains box's class probabilities for each output
         """
-        return None
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+        for i, output in enumerate(outputs):
+            anchors = self.anchors[i]
+            grid_height, grid_width = output.shape[:2]
+
+            t_xy = output[..., :2]
+            t_wh = output[..., 2:4]
+
+            sigmoid_conf = self.sigmoid(output[..., 4])
+            sigmoid_prob = self.sigmoid(output[..., 5:])
+
+            box_conf = np.expand_dims(sigmoid_conf, axis=-1)
+            box_class_prob = sigmoid_prob
+
+            box_confidences.append(box_conf)
+            box_class_probs.append(box_class_prob)
+
+            b_wh = anchors * np.exp(t_wh)
+            b_wh /= self.model.inputs[0].shape.as_list()[1:3]
+
+            grid = np.tile(np.indices((grid_width, grid_height)).T,
+                           anchors.shape[0]).reshape(
+                               (grid_height, grid_width) + anchors.shape)
+
+            b_xy = (self.sigmoid(t_xy) + grid) / [grid_width, grid_height]
+
+            b_xy1 = b_xy - (b_wh / 2)
+            b_xy2 = b_xy + (b_wh / 2)
+            box = np.concatenate((b_xy1, b_xy2), axis=-1)
+            box *= np.tile(np.flip(image_size, axis=0), 2)
+
+            boxes.append(box)
+        return (boxes, box_confidences, box_class_probs)
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
         """
@@ -130,4 +164,26 @@ class Yolo:
                     contains the box scores for each box
                         in filtered boxes
         """
-        return None
+        filtered_boxes = []
+        box_classes = []
+        box_scores = []
+
+        for i, b in enumerate(boxes):
+            box_conf = box_confidences[i]
+            box_class_prob = box_class_probs[i]
+
+            box_score = box_conf * box_class_prob
+
+            box_class = np.argmax(box_score, axis=-1)
+            box_class_score = np.max(box_score, axis=-1)
+
+            index = np.where(box_class_score >= self.class_t)
+
+            filtered_boxes.append(b[index])
+            box_classes.append(box_class[index])
+            box_scores.append(box_class_score[index])
+
+        filtered_boxes = np.concatenate(filtered_boxes)
+        box_classes = np.concatenate(box_classes)
+        box_scores = np.concatenate(box_scores)
+        return (filtered_boxes, box_classes, box_scores)
