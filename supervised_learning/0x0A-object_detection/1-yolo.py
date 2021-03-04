18680b363a550d593a168958647f81090bb58290
Kelsie Merchant <eislek02@gmail.com>
2021-03-04 14:52:35 -0600

    Add public method to class Yolo that processes outputs; from notes from global live coding over the project

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -96,4 +96,38 @@ class Yolo:
                     (grid_height, grid_width, anchor_boxes, classes)]:
                     contains box's class probabilities for each output
         """
-        return None
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+        for i, output in enumerate(outputs):
+            anchors = self.anchors[i]
+            grid_height, grid_weight = output.shape[:2]
+
+            t_xy = output[..., :2]
+            t_wh = output[..., 2:4]
+
+            sigmoid_conf = self.sigmoid(output[..., 4])
+            sigmoid_prob = self.sigmoid(output[..., 5:])
+
+            box_conf = np.expand_dims(sigmoid_conf, axis=-1)
+            box_class_prob = sigmoid_probs
+
+            box_confidences.append(box_conf)
+            box_class_probs.append(box_class_prob)
+
+            b_wh = anchors * np.exp(t_wh)
+            b_wh /= self.model.inputs[0].shape.as_list()[1:3]
+
+            grid = np.tile(np.indices((grid_weight, grid_height)).T,
+                           anchors.shape[0]).reshape(
+                               (grid_height, grid_weight) + anchors.shape)
+
+            b_xy = (self.sigmoid(t_xy) + grid) / [grid_width, grid_height]
+
+            b_xy1 = b_xy - (b_wh / 2)
+            b_xy2 = b_xy + (b_wh / 2)
+            box = np.concatenate((b_xy1, b_xy2), axis=-1)
+            box *= np.tile(np.flip(image_size, axis=0), 2)
+
+            boxes.append(box)
+        return (boxes, box_confidences, box_class_probs)
