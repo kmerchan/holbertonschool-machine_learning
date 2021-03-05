ff64fae590068073a5ad148920eccda1268d20cc
Kelsie Merchant <eislek02@gmail.com>
2021-03-05 09:50:52 -0600

    Initial commit with type check

--- supervised_learning/0x0C-neural_style_transfer/7-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/7-neural_style.py
@@ -47,6 +47,8 @@ class NST:
             calculates the style cost for generated image
         def content_cost(self, content_output):
             calculates the content cost for the generated image
+        def total cost(self, generated_image):
+            calculates the total cost for the generated image
     """
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
@@ -242,6 +244,20 @@ class NST:
 
     def total_cost(self, generated_image):
         """
-        Calculates the total cost
+        Calculates the total cost for the generated image
+
+        parameters:
+            generated_image [tf.Tensor of shape (1, nh, nw, 3)]:
+                contains the generated image
+
+        returns:
+            (J, J_content, J_style) [tuple]:
+                J: total cost
+                J_content: content cost
+                J_style?: style cost
         """
-        pass
+        shape = self.content_image.shape
+        if not is_instance(generated_image, (tf.Tensor, tf.Variable)) or \
+           generated_image.shape != shape:
+            raise TypeError(
+                "generated_image must be a tensor of shape {}".format(shape))
