Update to fix errors found while testing in sandbox; refactor scaling image method to both rescale and resize, change TensorFlow method for Bicubic interpolation

supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -4,6 +4,7 @@ Defines class NST that performs tasks for neural style transfer
 """
 
 
+import numpy as np
 import tensorflow as tf
 
 
@@ -54,7 +55,7 @@ class NST:
            len(style_image.shape) != 3:
             raise TypeError(
                 "style_image must be a numpy.ndarray with shape (h, w, 3)")
-        if type(content_image) is not np.ndarry or \
+        if type(content_image) is not np.ndarray or \
            len(content_image.shape) != 3:
             raise TypeError(
                 "content_image must be a numpy.ndarray with shape (h, w, 3)")
@@ -66,15 +67,15 @@ class NST:
         if content_h <= 0 or content_w <= 0 or content_c != 3:
             raise TypeError(
                 "content_image must be a numpy.ndarray with shape (h, w, 3)")
-        if type(alpha) is not float or type(alpha) is not int or alpha < 0:
+        if (type(alpha) is not float and type(alpha) is not int) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
-        if type(beta) is not float or type(beta) is not int or beta < 0:
+        if (type(beta) is not float and type(beta) is not int) or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
-        tf.executing_eagerly() = True
+        tf.enable_eager_execution()
 
         self.style_image = self.scale_image(style_image)
-        self.content_image = self.content_image(content_image)
+        self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
 
@@ -104,5 +105,18 @@ class NST:
         if h <= 0 or w <= 0 or c != 3:
             raise TypeError(
                 "image must be a numpy.ndarray with shape (h, w, 3)")
-        resized = tf.raw_ops.ResizeBicubic(image, size=(512,))
-        return (resized)
+        if h > w:
+            h_new = 512
+            w_new = int(w * (512 / h))
+        else:
+            w_new = 512
+            h_new = int(h * (512 / w))
+
+        resized = tf.image.resize_bicubic(tf.expand_dims(image, 0),
+                                          size=(h_new, w_new))
+        rescaled = resized / 255
+        rescaled = tf.div(tf.subtract(rescaled,
+                                      tf.reduce_min(rescaled)),
+                          tf.subtract(tf.reduce_max(rescaled),
+                                      tf.reduce_min(rescaled)))
+        return (rescaled)