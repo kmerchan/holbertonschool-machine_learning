5f80088ed55ff322b1b23b590e483b019c007847
Kelsie Merchant <eislek02@gmail.com>
2021-03-08 13:20:09 +0000

    Update to only use dropout on tanh activation layers (all but last layer) when calculating dz; remove self from variables to update gradient descent calculation from class method to individual function

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -36,21 +36,24 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     back = {}
     for index in range(L, 0, -1):
         A = cache["A{}".format(index - 1)]
-        if index == self.L:
+        if index == L:
             back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
+            dz = back["dz{}".format(index)]
+            
         else:
             dz_prev = back["dz{}".format(index + 1)]
             A_current = cache["A{}".format(index)]
             back["dz{}".format(index)] = (
                 np.matmul(W_prev.transpose(), dz_prev) *
                 (A_current * (1 - A_current)))
-        dz = back["dz{}".format(index)]
-        dz *= cache["D{}".format(index)]
-        dz /= keep_prob
+            dz = back["dz{}".format(index)]
+            dz *= cache["D{}".format(index)]
+            dz /= keep_prob
+
         dW = (1 / m) * (np.matmul(dz, A.transpose()))
         db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        W_prev = self.weights["W{}".format(index)]
-        self.__weights["W{}".format(index)] = (
-            self.weights["W{}".format(index)] - (alpha * dW))
-        self.__weights["b{}".format(index)] = (
-            self.weights["b{}".format(index)] - (alpha * db))
+        W_prev = weights["W{}".format(index)]
+        weights["W{}".format(index)] = (
+            weights["W{}".format(index)] - (alpha * dW))
+        weights["b{}".format(index)] = (
+            weights["b{}".format(index)] - (alpha * db))
