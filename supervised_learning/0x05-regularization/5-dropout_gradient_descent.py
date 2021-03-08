abe28c512ba108853abb4dd883e502c639002e39
Kelsie Merchant <eislek02@gmail.com>
2021-03-08 07:09:40 -0600

    Add function to calculate back propagation with dropout regularization using gradient descent

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Initial placeholder
+Defines function that updates the weights with Dropout regularization
+using gradient descent
 """
 
 import numpy as np
@@ -8,6 +9,48 @@ import numpy as np
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Initial placeholder
+    Updates the weights with Dropout regularization using gradient descent
+
+    parameters:
+        Y [one-hot numpy.ndarray of shape (classes, m)]:
+            contains the correct labels for the data
+            classes: number of classes
+            m: number of data points
+        weights [dict]:
+            contains the weights and biases of the network
+        cache [dict]:
+            contains the outputs and dropout masks of each layer
+        alpha [float]:
+            learning rate
+        keep_prob [float]:
+            the probability that a node will be kept
+        L [int]:
+            number of layers in the network
+
+    all layers should use the tanh activation function except last
+    last layer should use softmax activation function
+
+    the weights of the network should be updated in place
     """
-    pass
+    m = Y.shape[1]
+    back = {}
+    for index in range(L, 0, -1):
+        A = cache["A{}".format(index - 1)]
+        if index == self.L:
+            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
+        else:
+            dz_prev = back["dz{}".format(index + 1)]
+            A_current = cache["A{}".format(index)]
+            back["dz{}".format(index)] = (
+                np.matmul(W_prev.transpose(), dz_prev) *
+                (A_current * (1 - A_current)))
+        dz = back["dz{}".format(index)]
+        dz *= cache["D{}".format(index)]
+        dz /= keep_prob
+        dW = (1 / m) * (np.matmul(dz, A.transpose()))
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+        W_prev = self.weights["W{}".format(index)]
+        self.__weights["W{}".format(index)] = (
+            self.weights["W{}".format(index)] - (alpha * dW))
+        self.__weights["b{}".format(index)] = (
+            self.weights["b{}".format(index)] - (alpha * db))
