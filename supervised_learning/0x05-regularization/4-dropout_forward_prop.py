01c13a509ff34f40d40a740e7f73d499d21ae369
Kelsie Merchant <eislek02@gmail.com>
2021-03-08 06:50:49 -0600

    Add function that calculates forward propagation with dropout

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Initial placeholder
+Defines function that conducts forward propagation using Dropout
 """
 
 
@@ -9,6 +9,41 @@ import numpy as np
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
-    Initial placeholder
+    Conducts forward propagation using Dropout
+
+    parameters:
+        X [numpy.ndarray of shape(nx, m)]:
+            contains the input data for the network
+            nx: number of input features
+            m: number of data points
+        weights [dict]:
+            contains weights and biases of the network
+        L [int]:
+            number of layers in the network
+        keep_prob [float]:
+            probability that a node will be kept
+
+    all layers except last should use tanh activation function
+    last layer should use softmax activation function
+
+    returns:
+        dictionary containing the outputs of each layer and
+            the dropout mask used on each layer
     """
-    pass
+    outputs = {}
+    outputs["A0"] = X
+    for index in range(L):
+        weight = weights["W{}".format(index + 1)]
+        bias = weights["b{}".format(index + 1)]
+        z = np.matmul(weight, outputs["A{}".format(index)]) + bias
+        dropout = np.random.binomial(1, keep_prob, size=z.shape)
+        if index != (L - 1):
+            A = np.tanh(z)
+            A *= dropout
+            A /= keep_prob
+            outputs["D{}".format(index + 1)] = dropout
+        else:
+            A = np.exp(z)
+            A /= np.sum(A, axis=0, keepdims=True)
+        outputs["A{}".format(index + 1)] = A
+    return outputs
