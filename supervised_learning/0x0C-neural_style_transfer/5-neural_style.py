fe2a6e5311cdf94866fa68dd5a4fd17bd23661ff
Kelsie Merchant <eislek02@gmail.com>
2021-03-08 06:06:23 +0000

    Update method to compare style_outputs to gram_style_features to calculate style cost

--- supervised_learning/0x0C-neural_style_transfer/5-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/5-neural_style.py
@@ -263,5 +263,6 @@ class NST:
         style_cost = 0
         for i, layer in enumerate(self.style_layers):
             style_cost += (
-                weight * self.layer_style_cost(style_outputs[i], layer))
+                weight * self.layer_style_cost(style_outputs[i],
+                                               self.gram_style_features[i]))
         return style_cost
