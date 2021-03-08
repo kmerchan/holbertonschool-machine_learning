0f94437427df33f8b70805919970b93d8f64ee72
Kelsie Merchant <eislek02@gmail.com>
2021-03-07 23:36:25 -0600

    Revert back to previous code now that error has been tracked to scaling image method

--- supervised_learning/0x0C-neural_style_transfer/3-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/3-neural_style.py
@@ -197,14 +197,18 @@ class NST:
         Sets public instance attribute:
             gram_style_features and content_feature
         """
-        pre_style = tf.keras.applications.\
-            vgg19.preprocess_input(self.style_image * 255)
-        pre_content = tf.keras.applications.\
-            vgg19.preprocess_input(self.content_image * 255)
+        VGG19_model = tf.keras.applications.vgg19
+        preprocess_style = VGG19_model.preprocess_input(
+            self.style_image * 255)
+        preprocess_content = VGG19_model.preprocess_input(
+            self.content_image * 255)
 
-        style_features = self.model(pre_style)[:-1]
-        content_feature = self.model(pre_content)[-1]
+        style_features = self.model(preprocess_style)[:-1]
+        content_feature = self.model(preprocess_content)[-1]
 
-        self.gram_style_features = [self.gram_matrix(i)
-                                    for i in style_features]
+        gram_style_features = []
+        for feature in style_features:
+            gram_style_features.append(self.gram_matrix(feature))
+
+        self.gram_style_features = gram_style_features
         self.content_feature = content_feature
