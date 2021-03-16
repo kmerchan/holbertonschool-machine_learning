1a18ccb7d78411b946fc4c814270672d31a51301
Kelsie Merchant <eislek02@gmail.com>
2021-03-16 00:16:11 -0500

    Add function that determines the definiteness of a matrix; pushing to save work, but not currently working for negative definite

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -25,4 +25,26 @@ def definiteness(matrix):
     """
     if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    n = matrix.shape[0]
+    positive = 0
+    negative = 0
+    for i in range(n):
+        d_i = np.linalg.det(matrix[:(i + 1), :(i + 1)])
+        print("d_i: ", d_i)
+        if d_i > 0:
+            positive += 1
+        if d_i < 0:
+            negative += 1
+    if positive == n:
+        return ("Positive definite")
+    if negative == n:
+        return ("Negative definite")
+    if positive and negative and d_i != 0:
+        return ("Indefinite")
+    if not negative and d_i == 0:
+        return ("Positive semi-definite")
+    if not positive and d_i == 0:
+        return ("Negative semi-definite")
     return None
