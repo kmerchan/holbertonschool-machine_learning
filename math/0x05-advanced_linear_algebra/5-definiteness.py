5af3bf6df1b402c7b336d9322bfa280a59adc0f0
Kelsie Merchant <eislek02@gmail.com>
2021-03-18 11:20:49 -0500

    Change '==' to 'is' to fix pycodestyle error

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -26,7 +26,7 @@ def definiteness(matrix):
     if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
     if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1] or \
-       np.array_equal(matrix, matrix.T) == False:
+       np.array_equal(matrix, matrix.T) is False:
         return None
     positive = 0
     negative = 0
