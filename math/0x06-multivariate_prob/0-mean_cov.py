bc05d43b272415b10e7c6898ef44365c87d697af
Kelsie Merchant <eislek02@gmail.com>
2021-03-22 10:10:02 -0500

    Initial commit of function to calculate the mean and covariance of a data set, including type and value checks

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates the mean and covariance of a data set
+"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Calculates the mean and covariance of a data set
+
+    parameters:
+        X [numpy.ndarray of shape (n, d)]: contains the data set
+            n: number of data points
+            d: number of dimensions in each data point
+
+    not allowed to use numpy.cov
+
+    returns:
+        mean, cov [tuple]:
+            mean [numpy.ndarray of shape (1, d)]:
+                containing the mean of the data set
+            cov [numpy.ndarray of shape (d, d)]:
+                containing the covariance matrix of the data set
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
