0f90620eb07db07457c78a3547435137dfeb8a2b
Kelsie Merchant <eislek02@gmail.com>
2021-03-22 10:10:32 -0500

    Initial commit of function to calculate a correlation matrix, including type and value checks

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates a correlation matrix
+"""
+
+
+import numpy as np
+
+
+def correlation(C):
+    """
+    Calculates a correlation matrix
+
+    parameters:
+        C [numpy.ndarray of shape (d, d)]: contains a covariance matrix
+            d: number of dimensions
+
+    returns:
+        [numpy.ndarray of shape (d, d)]:
+            containing the correlation matrix
+    """
+    if type(C) is not np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2:
+        raise ValueError("C must be a 2D square matrix")
+    d, d_2 = C.shape
+    if d != d_2:
+        raise ValueError("C must be a 2D square matrix")
