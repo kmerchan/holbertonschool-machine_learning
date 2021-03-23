e3d62e12b304c9d8dce6096d36cac650eafd8618
Kelsie Merchant <eislek02@gmail.com>
2021-03-22 21:42:03 -0500

    Update covariance calculation to divide by (n - 1) instead of multiply by (1 / (n - 1))

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -41,7 +41,7 @@ class MultiNormal:
             raise ValueError("data must contain multiple data points")
         mean = np.mean(data, axis=1, keepdims=True)
         self.mean = mean
-        cov = (1 / (n - 1)) * np.matmul(data - mean, data.T - mean.T)
+        cov = np.matmul(data - mean, data.T - mean.T) / (n - 1)
         self.cov = cov
 
     def pdf(self, x):
