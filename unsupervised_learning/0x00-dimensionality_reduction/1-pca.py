86b215601a772a709d83497529ef6937e551b142
Kelsie Merchant <eislek02@gmail.com>
2021-03-23 01:26:29 -0500

    Initial commit of function that performs PCA on dataset

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Defines function that performs principal components analysis (PCA) on dataset
+"""
+
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Performs principal components analysis (PCA) on a dataset
+
+    parameters:
+        X [numpy.ndarray of shape (n, d)]: dataset
+            n: number of data points
+            d: number of dimensions in each data point
+        ndim [int]: the new dimensionality of the transformed X
+
+    returns:
+        T [numpy.ndarray of shape (n, ndim)]:
+            containing the transformed version of X
+            n: number of data points
+            ndim: the new dimensionality of the transformed X
+    """
+    return None
