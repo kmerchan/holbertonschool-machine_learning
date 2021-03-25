04ea5d00eb4a2a1152156d34e3d9572a239cfc97
Kelsie Merchant <eislek02@gmail.com>
2021-03-24 23:21:14 -0500

    Initial commit

---
+++ unsupervised_learning/0x00-dimensionality_reduction/7-cost.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Defines function that calculates the cost of the t-SNE transformation
+"""
+
+
+import numpy as np
+
+
+def cost(P, Q):
+    """
+    Calculates the cost of the t-SNE transformation
+
+    parameters:
+        P [numpy.ndarray of shape (n, n)]:
+            containing the P affinities
+        Q [numpy.ndarray of shape (n, n)]:
+            containing the Q affinities
+
+    returns:
+        c: the cost of the transformation
+    """
+    return None
