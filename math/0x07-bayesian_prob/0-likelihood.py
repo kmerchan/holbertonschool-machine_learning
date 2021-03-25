2b810252cb943ef36d7507e3fa3798541d82bad6
Kelsie Merchant <eislek02@gmail.com>
2021-03-25 06:13:02 -0500

    Initial commit of function to calculate likelihood, including type and value checks

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -23,3 +23,16 @@ def likelihood(x, n, P):
         a 1D numpy.ndarray containing the likelihood of obtaining the data,
             x and n, for each probability in P
     """
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    for value in P:
+        if value > 1 or value < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+    return (P)
