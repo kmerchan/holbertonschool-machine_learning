805734aa401c5a0ac5b8480ff278feb8579bd62e
Kelsie Merchant <eislek02@gmail.com>
2021-03-24 23:14:30 -0500

    Initial commit

---
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Defines function that calculates the Q affinities
+"""
+
+
+import numpy as np
+
+
+def Q_affinities(Y):
+    """
+    Calculates the Q affinitiest
+
+    parameters:
+        Y [numpy.ndarray of shape (n, ndim)]:
+            containing the low dimensional transformation of X (dataset)
+            n: the number of data points
+            ndim: the new dimensional representation of X
+
+    returns:
+        Q [numpy.ndarray of shape (n, n)]:
+            contatining the Q affinities
+        num [numpy.ndarray of shape (n, n)]:
+            containing the numerator of the Q affinities
+    """
+    return None
