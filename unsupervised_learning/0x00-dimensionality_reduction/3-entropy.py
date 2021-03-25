b52bab2e502d4d29d63eae63253332c5cebe7265
Kelsie Merchant <eislek02@gmail.com>
2021-03-24 23:05:05 -0500

    Initial commit

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Defines function that calculates the Shannon entropy and P affinities
+relative to a data point
+"""
+
+
+import numpy as np
+
+
+def HP(Di, beta):
+    """
+    Calculates the Shannon entropy and P affinities relative to a data point
+
+    parameters:
+        Di [numpy.ndarray of shape (n - 1,)]:
+            containing the pairwise distances between
+                a data point and all other points
+            n: the number of data points
+        beta [numpy.ndarray of shape (1,)]:
+            containing the beta value for the Gaussian distribution
+
+    returns:
+        (Hi, Pi)
+        Hi: the Shannon entropy of the points
+        Pi [numpy.ndarray of shape (n - 1,)]:
+            contatining the P affinities of the points
+    """
+    return None
