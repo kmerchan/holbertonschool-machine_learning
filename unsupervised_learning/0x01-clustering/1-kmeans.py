587bda472d3e14185e07d3de36cfc869d17bf402
Kelsie Merchant <eislek02@gmail.com>
2021-04-12 01:21:53 -0500

    Add 1-kmeans.py work in progress

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -42,4 +42,21 @@ def kmeans(X, k, iterations=1000):
                     that each data point belongs to
         or None, None on failure
     """
-    return None, None
+    # type checks to catch failure
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(k) is not int or k <= 0:
+        return None, None
+    if type(iterations) is not int or iterations <= 0:
+        return None, None
+    n, d = X.shape
+    # initialize cluster centroids using multivariate uniform distribution
+    low = np.min(X, axis=0)
+    high = np.max(X, axis=0)
+    C = np.random.uniform(low, high, size=(k, d))
+    # save copy of centroids to compare against later
+    save_centroids = np.copy(C)
+    if C.all() == saved_centroids.all():
+        return C, clss
+    saved_centroids = np.copy(C)
+    return C, clss
