c06fe7aedb201b304c2ad595b881c83d4f615b34
Kelsie Merchant <eislek02@gmail.com>
2021-04-12 01:25:05 -0500

    Initial commit

---
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Initial placeholder
+"""
+
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """
+    Initial placeholder
+    """
+    pass
