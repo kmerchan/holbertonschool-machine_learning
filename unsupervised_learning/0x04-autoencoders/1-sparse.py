80442678ddff6fe605bce6a591e426569d041fc0
Kelsie Merchant <eislek02@gmail.com>
2021-04-25 16:36:28 -0500

    Add sparse autoencoder with L1 regularizer on the encoder

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -51,4 +51,37 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     if type(lambtha) is not float:
         raise TypeError("lambtha must be float representing the \
         regularization parameter used for L1 regularization")
-    return None, None, None
+
+    # encoder
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    encoder_value = encoder_inputs
+    for i in range(len(hidden_layers)):
+        encoder_layer = keras.layers.Dense(units=hidden_layers[i],
+                                           activation='relu')
+        encoder_value = encoder_layer(encoder_value)
+    regularizer = keras.regularizers.l1(lambtha)
+    encoder_output_layer = keras.layers.Dense(units=latent_dims,
+                                              activation='relu',
+                                              activity_regularizer=regularizer)
+    encoder_outputs = encoder_output_layer(encoder_value)
+    encoder = keras.Model(inputs=encoder_inputs, outputs=encoder_outputs)
+
+    # decoder
+    decoder_inputs = keras.Input(shape=(latent_dims,))
+    decoder_value = decoder_inputs
+    for i in range(len(hidden_layers) - 1, -1, -1):
+        decoder_layer = keras.layers.Dense(units=hidden_layers[i],
+                                           activation='relu')
+        decoder_value = decoder_layer(decoder_value)
+    decoder_output_layer = keras.layers.Dense(units=input_dims,
+                                              activation='sigmoid')
+    decoder_outputs = decoder_output_layer(decoder_value)
+    decoder = keras.Model(inputs=decoder_inputs, outputs=decoder_outputs)
+
+    # autoencoder
+    inputs = encoder_inputs
+    auto = keras.Model(inputs=inputs, outputs=decoder(encoder(inputs)))
+    auto.compile(optimizer='adam',
+                 loss='binary_crossentropy')
+
+    return encoder, decoder, auto
