7408e33806b18ea01cb0143dc853e5cc476db06e
Kelsie Merchant <eislek02@gmail.com>
2021-04-29 23:39:59 -0500

    Initial commit

---
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defines function that performs forward propagation for simple RNN
+"""
+
+
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """
+    Performs forward propagation for simple RNN
+    """
+    return None, None
---
+++ supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Defines the class GRUCell that represents a gated recurrent unit
+"""
+
+
+import numpy as np
+
+
+class GRUCell:
+    """
+    Represents a gated recurrent unit
+
+    class constructor:
+        def __init__(self, i, h, o)
+
+    public instance attributes:
+        Wz: update gate weights
+        bz: update gate biases
+        Wr: reset gate weights
+        br: reset gate biases
+        Wh: intermediate hidden state and input data weights
+        bh: intermediate hidden state and input data biases
+        Wy: output weights
+        by: output biases
+
+    public instance methods:
+        def forward(self, h_prev, x_t):
+            performs forward propagation for one time step
+    """
+    def __init__(self, i, h, o):
+        """
+        Class constructor
+
+        parameters:
+            i: dimensionality of the data
+            h: dimensionality of the hidden state
+            o: dimensionality of the outputs
+
+        creates public instance attributes:
+            Wz: update gate weights
+            bz: update gate biases
+            Wr: reset gate weights
+            br: reset gate biases
+            Wh: intermediate hidden state and input data weights
+            bh: intermediate hidden state and input data biases
+            Wy: output weights
+            by: output biases
+
+        weights should be initialized using random normal distribution
+        weights will be used on the right side for matrix multiplication
+        biases should be initiliazed as zeros
+        """
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+        self.Wh = np.random.normal(size=(h + i, h))
+        self.Wy = np.random.normal(size=(h, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Performs forward propagation for one time step
+
+        parameters:
+            h_prev [numpy.ndarray of shape (m, h)]:
+                contains previous hidden state
+                m: the batch size for the data
+                h: dimensionality of hidden state
+            x_t [numpy.ndarray of shape (m, i)]:
+                contains data input for the cell
+                m: the batch size for the data
+                i: dimensionality of the data
+
+        output of the cell should use softmax activation function
+
+        returns:
+            h_next, y:
+            h_next: the next hidden state
+            y: the output of the cell
+        """
+        return None, None
