4c3c2a0984742c9f696ae812134c85cfda4c1641
Kelsie Merchant <eislek02@gmail.com>
2021-04-29 23:48:25 -0500

    Initial commit

---
+++ supervised_learning/0x0D-RNNs/4-deep_rnn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defines function that performs forward propagation for a deep RNN
+"""
+
+
+import numpy as np
+
+
+def deep_rnn(rnn_cells, X, h_0):
+    """
+    Performs forward propagation for a deep RNN
+    """
+    return None, None
