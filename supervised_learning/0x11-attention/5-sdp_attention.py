35c119ca40de158aa005de7a75a5fd358d29c41a
Kelsie Merchant <eislek02@gmail.com>
2021-05-19 23:13:43 -0500

    Initial commit

---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates the scaled dot product attention
+"""
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """
+    Calculates the scaled dot product attention
+
+    parameters:
+        Q [tensor with last two dimensions as (..., seq_len_q, dk)]:
+            contains the query matrix
+        K [tensor with last two dimensions as (..., seq_len_v, dk)]:
+            contains the key matrix
+        V [tensor with last two dimensions as (..., seq_len_v, dv)]:
+            contains the value matrix
+        mask [tensor that can be broadcast into (..., seq_len_q, seq_len_v)]:
+            contains the optional mask, or defaulted to None
+
+    returns:
+        outputs, weights:
+            outputs [tensor with last two dimensions as (..., seq_len_q, dv)]:
+                contains the scaled dot product attention
+            weights [tensor with last two dimensions as
+                    (..., seq_len_q, seq_len_v)]:
+                contains the attention weights
+    """
+    if mask is not None:
+        mask *= -1e9
+    return None, None
