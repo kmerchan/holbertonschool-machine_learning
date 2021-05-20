f10b2ca365f94fb1697f5f780639eef422818196
Kelsie Merchant <eislek02@gmail.com>
2021-05-19 23:01:30 -0500

    Initial commit

---
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates the positional encoding for a transformer
+"""
+
+
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """
+    Calculates the positional encoding for a transformer
+
+    parameters:
+        max_seq_len [int]:
+            represents the maximum sequence length
+        dm: model depth
+
+    returns:
+        [numpy.ndarray of shape (max_seq_len, dm)]:
+            contains the positional encoding vectors
+    """
+    return None
