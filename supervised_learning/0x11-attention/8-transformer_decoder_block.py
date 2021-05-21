273bb698accdea879f0c4759c14f3cd9dad536e9
Kelsie Merchant <eislek02@gmail.com>
2021-05-21 01:51:01 -0500

    Add class that creates decoder block for transformer

--- supervised_learning/0x11-attention/8-transformer_decoder_block.py
+++ supervised_learning/0x11-attention/8-transformer_decoder_block.py
@@ -71,16 +71,18 @@ class DecoderBlock(tf.keras.layers.Layer):
         if type(drop_rate) is not float:
             raise TypeError(
                 "drop_rate must be float representing dropout rate")
+        super(DecoderBlock, self).__init__()
         self.mha1 = MultiHeadAttention(dm, h)
         self.mha2 = MultiHeadAttention(dm, h)
-        self.dense_hidden = None
-        self.dense_output = None
-        self.layernorm1 = None
-        self.layernorm2 = None
-        self.layernorm3 = None
-        self.dropout1 = None
-        self.dropout2 = None
-        self.dropout3 = None
+        self.dense_hidden = tf.keras.layers.Dense(units=hidden,
+                                                  activation='relu')
+        self.dense_output = tf.keras.layers.Dense(units=dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
         """
@@ -102,4 +104,18 @@ class DecoderBlock(tf.keras.layers.Layer):
             [tensor of shape (batch, target_seq_len, dm)]:
                 contains the block's output
         """
-        return None
+        attention_output1, _ = self.mha1(x, x, x, look_ahead_mask)
+        attention_output1 = self.dropout1(attention_output1, training=training)
+        output1 = self.layernorm1(x + attention_output1)
+
+        attention_output2, _ = self.mha2(output1, encoder_output,
+                                         encoder_output, padding_mask)
+        attention_output2 = self.dropout2(attention_output2, training=training)
+        output2 = self.layernorm2(output1 + attention_output2)
+
+        dense_output = self.dense_hidden(output2)
+        ffn_output = self.dense_output(dense_output)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        output3 = self.layernorm3(output2 + ffn_output)
+
+        return output3
