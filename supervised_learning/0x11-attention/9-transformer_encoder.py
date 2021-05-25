0b3dd9b37fc4a93144df42c52b40614ab6e14f47
Kelsie Merchant <eislek02@gmail.com>
2021-05-24 20:18:23 -0500

    Update method to get shape

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -110,7 +110,7 @@ class Encoder(tf.keras.layers.Layer):
             [tensor of shape (batch, input_seq_len, dm)]:
                 contains the encoder output
         """
-        seq_len = x.get_shape().as_list()[1]
+        seq_len = x.shape[1]
 
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
