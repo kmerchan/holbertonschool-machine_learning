64b6f8db9f9958428d825c83e284423dd2775f79
Kelsie Merchant <eislek02@gmail.com>
2021-05-27 22:50:49 -0500

    Add function that creates and trains a gensim word2vec model

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -39,7 +39,7 @@ def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
         the trained model
     """
     model = Word2Vec(sentences=sentences,
-                     vector_size=size,
+                     size=size,
                      min_count=min_count,
                      window=window,
                      negative=negative,
@@ -47,5 +47,7 @@ def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
                      iter=iterations,
                      seed=seed,
                      workers=workers)
-    model.train(sentences)
+    model.train(sentences,
+                total_examples=model.corpus_count,
+                epochs=model.epochs)
     return model
