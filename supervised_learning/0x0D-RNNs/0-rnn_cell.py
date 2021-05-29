3b19a1c1135abbf7c0122f2300f8ace4a6be1e7f
Kelsie Merchant <eislek02@gmail.com>
2021-05-29 00:19:41 -0500

    Add class RNNCell that represents a single cell of a simple RNN

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -48,6 +48,20 @@ class RNNCell:
         self.Wh = np.random.normal(size=(h + i, h))
         self.Wy = np.random.normal(size=(h, o))
 
+    def softmax(self, x):
+        """
+        Performs the softmax function
+
+        parameters:
+            x: the value to perform softmax on to generate output of cell
+
+        return:
+            softmax of x
+        """
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        softmax = e_x / e_x.sum(axis=1, keepdims=True)
+        return softmax
+
     def forward(self, h_prev, x_t):
         """
         Performs forward propagation for one time step
@@ -69,4 +83,7 @@ class RNNCell:
             h_next: the next hidden state
             y: the output of the cell
         """
-        return None, None
+        concatenation = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concatenation, self.Wh) + self.bh)
+        y = self.softmax(np.matmul(h_next, self.Wy) + self.by)
+        return h_next, y
