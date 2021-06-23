eeaa8cb022b6b70be49f1c91c0f4f3efa4f69264
Kelsie Merchant <eislek02@gmail.com>
2021-06-22 22:19:41 -0500

    Add function that performs Q learning

--- reinforcement_learning/0x00-q_learning/3-q_learning.py
+++ reinforcement_learning/0x00-q_learning/3-q_learning.py
@@ -16,4 +16,36 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
     returns:
         Q, total_rewards
     """
-    return None, None
+    total_rewards = []
+    max_epsilon = epsilon
+    for episode in range(episodes):
+        current_state = env.reset()
+        done = False
+
+        total_episode_reward = 0
+
+        for step in range(max_steps):
+            p = np.random.uniform(0, 1)
+            if p < epsilon:
+                # exploring
+                action = np.random.randint(Q.shape[1])
+            else:
+                # exploiting
+                action = np.argmax(Q[state, :])
+            next_state, reward, done, _ = env.step(action)
+
+            Q[current_state, action] = (
+                Q[current_state, action] * (1 - alpha) + alpha *
+                (reward + gamma * np.max(Q[new_state, :])))
+            total_episode_reward += reward
+
+            if done:
+                break
+
+            current_state = next_state
+
+        epsilon = (min_epsilon + (max_epsilon - min_epsilon) *
+                   np.exp(-epsilon_decay * episode))
+        total_rewards.append(total_episode_reward)
+
+    return Q, total_rewards
