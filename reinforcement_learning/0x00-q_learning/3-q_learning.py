3ab36669b53b5fec7758191a4c7f4a0d110b2438
Kelsie Merchant <eislek02@gmail.com>
2021-06-23 03:23:18 +0000

    Fix name error from epsilon-greedy function transfer into q-learning function; Python program runs, but output is not matching expected yet

--- reinforcement_learning/0x00-q_learning/3-q_learning.py
+++ reinforcement_learning/0x00-q_learning/3-q_learning.py
@@ -31,12 +31,12 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
                 action = np.random.randint(Q.shape[1])
             else:
                 # exploiting
-                action = np.argmax(Q[state, :])
+                action = np.argmax(Q[current_state, :])
             next_state, reward, done, _ = env.step(action)
 
             Q[current_state, action] = (
                 Q[current_state, action] * (1 - alpha) + alpha *
-                (reward + gamma * np.max(Q[new_state, :])))
+                (reward + gamma * np.max(Q[current_state, :])))
             total_episode_reward += reward
 
             if done:
