bbd4ca08ac6efe8c4c6b04b4fb15386e331e6b5d
Kelsie Merchant <eislek02@gmail.com>
2021-06-22 20:24:28 -0500

    Add function that utilizes epsilon-greedy to determine the next action

---
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Defines function that uses epsilon-greedy to determine the next action
+"""
+
+
+import gym
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    Uses epsilon-greedy to determine the next action
+
+    returns:
+        the next action index
+    """
+    p = np.random.uniform(0, 1)
+    if p < epsilon:
+        # exploring
+        action = np.random.randint(Q.shape[1])
+    else:
+        # exploiting
+        action = np.argmax([state, :])
+    return action
