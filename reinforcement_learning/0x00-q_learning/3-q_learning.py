49040a8f6d04e419378ccd62f080cb3c330d2cb2
Kelsie Merchant <eislek02@gmail.com>
2021-06-22 20:34:04 -0500

    Initial commit of file set-up

---
+++ reinforcement_learning/0x00-q_learning/3-q_learning.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Defines function that performs Q-learning
+"""
+
+
+import gym
+import numpy as np
+
+
+def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
+          epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+    """
+    Performs Q-learning
+
+    returns:
+        Q, total_rewards
+    """
+    return None, None
---
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Defines function that has trained agent play an episode
+"""
+
+
+import gym
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """
+    Has trained agent play an episode
+
+    returns:
+        total rewards for the episode
+    """
+    return None
