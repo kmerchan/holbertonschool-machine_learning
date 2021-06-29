6e8ee8898640aec0583b09a59f68b07ad683823e
Kelsie Merchant <eislek02@gmail.com>
2021-06-29 17:03:58 -0500

    Initial commit of file set-up

---
+++ reinforcement_learning/0x01-deep_q_learning/play.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Script that displays a game played by the agent trained in `train.py`
+to play Atari's Breakout
+"""
+
+
+import gym
+import tensorflow.keras as K
+import numpy as np
+
+
+def playing():
+    """
+    Displays a game played by the agent trained to play Atari's Breakout
+    """
+    return None
+
+
+if __name__ == '__main__':
+    playing()
---
+++ reinforcement_learning/0x01-deep_q_learning/train.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Script that trains an agent that can play Atari's Breakout
+"""
+
+
+import gym
+import tensorflow.keras as K
+import numpy as np
+
+
+def training():
+    """
+    Trains an agent to play Atari's Breakout
+    """
+    return None
+
+
+if __name__ == '__main__':
+    training()
