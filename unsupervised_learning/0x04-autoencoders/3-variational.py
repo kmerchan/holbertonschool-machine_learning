3f133622cdb1c82d26f53d637294a2626f6f041e
Kelsie Merchant <eislek02@gmail.com>
2021-07-08 13:53:17 -0500

    Add progress on Variational Autoencoder

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -48,4 +48,46 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     if type(latent_dims) is not int:
         raise TypeError("latent_dims must be an int containing dimensions of \
         latent space representation")
-    return None, None, None
+
+    # encoder
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    encoder_value = encoder_inputs
+    for i in range(len(hidden_layers)):
+        encoder_layer = keras.layers.Conv2D(hidden_layers[i],
+                                            activation='relu',
+                                            kernel_size=(3, 3),
+                                            padding='same')
+        encoder_value = encoder_layer(encoder_value)
+        encoder_batch_norm = keras.layers.BatchNormalization()
+        encoder_value = encoder_batch_norm(encoder_value)
+    encoder_flatten = keras.layers.Flatten()
+    encoder_value = encoder_flatten(encoder_value)
+    encoder_dense = keras.layers.Dense(activation='relu')
+    encoder_value = encoder_dense(encoder_value)
+    encoder_batch_norm = keras.layers.BatchNormalization()
+    encoder_value = encoder_batch_norm(encoder_value)
+
+    encoder_output_layer = keras.layers.Dense(units=latent_dims,
+                                              activation='relu')
+    encoder_outputs = encoder_output_layer(encoder_value)
+    encoder = keras.Model(inputs=encoder_inputs, outputs=encoder_outputs)
+
+    # decoder
+    decoder_inputs = keras.Input(shape=(latent_dims,))
+    decoder_value = decoder_inputs
+    for i in range(len(hidden_layers) - 1, -1, -1):
+        decoder_layer = keras.layers.Dense(units=hidden_layers[i],
+                                           activation='relu')
+        decoder_value = decoder_layer(decoder_value)
+    decoder_output_layer = keras.layers.Dense(units=input_dims,
+                                              activation='sigmoid')
+    decoder_outputs = decoder_output_layer(decoder_value)
+    decoder = keras.Model(inputs=decoder_inputs, outputs=decoder_outputs)
+
+    # autoencoder
+    inputs = encoder_inputs
+    auto = keras.Model(inputs=inputs, outputs=decoder(encoder(inputs)))
+    auto.compile(optimizer='adam',
+                 loss='binary_crossentropy')
+
+    return encoder, decoder, auto
