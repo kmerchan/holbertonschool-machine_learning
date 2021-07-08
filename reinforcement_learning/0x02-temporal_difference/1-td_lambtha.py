2fa5348db2ab213f8ffe2fcd057ba2a4b97f8c0f
Kelsie Merchant <eislek02@gmail.com>
2021-07-07 23:56:40 -0500

    Add comments for readability

--- reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
@@ -25,28 +25,41 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
     returns:
         V: the updated value estimate
     """
+    # episode = [[states], [rewards]]
     episode = [[], []]
+    # set up eligibility traces as a list initialized to 0
     Et = [0 for i in range(env.observation_space.n)]
     for ep in range(episodes):
+        # the initial state comes from resetting the environment
         state = env.reset()
+        # iterate until done or max number of steps per episode reached
         for step in range(max_steps):
+            # list of eligibility traces calculated with lambda & gamma
             Et = list(np.array(Et) * lambtha * gamma)
+            # update list by increasing Et at current state
             Et[state] += 1
 
+            # get action from the current state using policy
             action = policy(state)
+            # perform the action to get next_state, reward, done, and info
             next_state, reward, done, info = env.step(action)
 
+            # if the algorithm finds a hole, the reward is updated to -1
             if env.desc.reshape(env.observation_space.n)[next_state] == b'H':
                 reward = -1
-
+            # if the algorithm finds the goal, the reward is updated to 1
             if env.desc.reshape(env.observation_space.n)[next_state] == b'G':
                 reward = 1
 
+            # delta = reward + (gamma * V[next_state]) - V[state]
             delta_t = reward + gamma * V[next_state] - V[state]
-
+            # V[state] = V[state] + (alpha * delta * eligibility trace[state])
             V[state] = V[state] + alpha * delta_t * Et[state]
 
+            # break if done to trigger return
             if done:
                 break
+            # otherwise, update state to next_state and continue
             state = next_state
+    # return V as numpy array when finished
     return np.array(V)
