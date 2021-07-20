9c35fe32fd9fd1ba4f3158848b26ce9e04363647
Kelsie Merchant <eislek02@gmail.com>
2021-07-19 20:49:12 -0500

    Add script that sorts a Pandas DataFrame by the High price in descending order

--- pipeline/0x00-pandas/7-high.py
+++ pipeline/0x00-pandas/7-high.py
@@ -8,6 +8,6 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df = # YOUR CODE HERE
+df = df.sort_values(by='High', ascending=False)
 
 print(df.head())
