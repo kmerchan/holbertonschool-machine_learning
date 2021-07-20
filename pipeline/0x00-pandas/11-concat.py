190f0aaba3f8da54429c99222c513109e2a78e0b
Kelsie Merchant <eislek02@gmail.com>
2021-07-20 15:46:47 -0500

    Add script that creates two Pandas DataFrames, indexes them on Timestamp column, and concatenates the DataFrames with key labels

--- pipeline/0x00-pandas/11-concat.py
+++ pipeline/0x00-pandas/11-concat.py
@@ -9,8 +9,11 @@ from_file = __import__('2-from_file').from_file
 df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-# YOUR CODE HERE
+df2 = df2.loc[df2['Timestamp'] <= 1417411920]
 
-df = # YOUR CODE HERE
+df1 = df1.set_index('Timestamp')
+df2 = df2.set_index('Timestamp')
+
+df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
 print(df)
