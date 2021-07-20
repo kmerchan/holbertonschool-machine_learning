e0f556680c6518b00fee87dff65d1847e101c1b5
Kelsie Merchant <eislek02@gmail.com>
2021-07-19 19:53:16 -0500

    Add script to create Pandas DataFrame from a dictionary; rename file

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -20,8 +20,13 @@ def from_dictionary():
     returns:
         the newly created pd.DataFrame
     """
-    return None
+    df = pd.DataFrame(
+        {
+            "First": [0.0, 0.5, 1.0, 1.5],
+            "Second": ["one", "two", "three", "four"]
+        },
+        index=list("ABCD"))
+    return df
 
 
-if __name__ == "__main__":
-    df = from_dictionary()
+df = from_dictionary()
