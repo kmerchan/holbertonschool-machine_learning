9c7000d37b91d94d7887f4491a206e22ba7bbab3
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 12:03:14 -0500

    Initial commit of script that uses the GitHub API to print the location of a specific user

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Uses the GitHub API to print the location of a specific user,
+where user is passed as first argument of the script with full API URL
+
+ex) "./2-user_location.py https://api.github.com/users/holbertonschool"
+"""
+
+
+if __name__ == "__main__":
+    print("Not found")
