3462c30d8518606031300c40b05bd829ccc138b0
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 11:58:41 -0500

    Initial commit of method using Star Wars API to create list of home planets of sentient species

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Defines methods to ping the Star Wars API and return the list of home planets
+for all sentient species
+"""
+
+
+def sentientPlanets():
+    """
+    Uses the Star Wars API to return the list of home planets
+        for all sentient species
+
+    returns:
+        [list]: home planets of sentient species
+    """
+    homePlanets = []
+    return homePlanets
