d8826a4ed27341f616121b3fced0980e7ce36bf8
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 13:39:34 -0500

    Add method that returns the list of ships that can hold a given number of passengers

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -5,6 +5,9 @@ that can hold a given number of passengers
 """
 
 
+import requests
+
+
 def availableShips(passengerCount):
     """
     Uses the Star Wars API to return the list of ships that can hold
@@ -23,5 +26,16 @@ def availableShips(passengerCount):
     if passengerCount < 0:
         raise ValueError(
             "passengerCount must be a positive number of passengers")
+    url = "https://swapi-api.hbtn.io/api/starships/?format=json"
+    ships = []
+    while url:
+        results = requests.get(url).json()
+        ships += results.get('results')
+        url = results.get('next')
     shipsList = []
+    for ship in ships:
+        passengers = ship.get('passengers').replace(",", "")
+        if passengers != "n/a" and passengers != "unknown":
+            if int(passengers) >= passengerCount:
+                shipsList.append(ship.get('name'))
     return shipsList
