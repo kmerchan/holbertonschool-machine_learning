8e7a8b761c86fe4dd983f1f2a6f5294ad249bf06
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 18:06:08 -0500

    Add script that prints the location of a specific GitHub user utilizing GitHub's API

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -7,5 +7,31 @@ ex) "./2-user_location.py https://api.github.com/users/holbertonschool"
 """
 
 
+import requests
+from sys import argv
+from time import time
+
+
 if __name__ == "__main__":
-    print("Not found")
+    if len(argv) < 2:
+        raise TypeError(
+            "Input must have the full API URL passed in as an argument: {}{}".
+            format('ex. "./2-user_location.py',
+                   'https://api.github.com/users/holbertonschool"'))
+    try:
+        url = argv[1]
+        results = requests.get(url)
+        if results.status_code == 403:
+            reset = results.headers.get('X-Ratelimit-Reset')
+            waitTime = int(reset) - time()
+            minutes = round(waitTime / 60)
+            print('Reset in {} min'.format(minutes))
+        else:
+            results = results.json()
+            location = results.get('location')
+            if location:
+                print(location)
+            else:
+                print('Not found')
+    except Exception as err:
+        print('Not found')
