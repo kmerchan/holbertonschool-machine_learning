1ae197c2c19194b7397ecfc42e2621bc6d09046c
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 14:09:26 -0500

    Add method that returns the list of names of the home planets of all sentient species

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -5,6 +5,9 @@ for all sentient species
 """
 
 
+import requests
+
+
 def sentientPlanets():
     """
     Uses the Star Wars API to return the list of home planets
@@ -13,5 +16,18 @@ def sentientPlanets():
     returns:
         [list]: home planets of sentient species
     """
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
     homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
     return homePlanets
