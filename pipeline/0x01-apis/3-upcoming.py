56706af7fde82b9b04de60f25573ea286fc890e1
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 21:50:23 -0500

    Add script to display the upcoming launch information using the unofficial SpaceX API

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -8,11 +8,33 @@ and if 2 launches have the same date, it's the first one in the API result.
 """
 
 
+import requests
+
+
 if __name__ == "__main__":
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    results = requests.get(url).json()
+    dateCheck = float('inf')
     launchName = None
-    date = None
     rocket = None
     launchPad = None
     location = None
+    for launch in results:
+        launchDate = launch.get('date_unix')
+        if launchDate < dateCheck:
+            dateCheck = launchDate
+            date = launch.get('date_local')
+            launchName = launch.get('name')
+            rocket = launch.get('rocket')
+            launchPad = launch.get('launchpad')
+    if rocket:
+        rocket = requests.get('https://api.spacexdata.com/v4/rockets/{}'.
+                              format(rocket)).json().get('name')
+    if launchPad:
+        launchpad = requests.get('https://api.spacexdata.com/v4/launchpads/{}'.
+                                 format(launchPad)).json()
+        launchPad = launchpad.get('name')
+        location = launchpad.get('locality')
+
     print("{} ({}) {} - {} ({})".format(
         launchName, date, rocket, launchPad, location))
