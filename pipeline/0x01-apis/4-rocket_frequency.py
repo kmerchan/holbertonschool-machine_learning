c67467bc45c8b2cc7976374cae49d639e4e03a5b
Kelsie Merchant <eislek02@gmail.com>
2021-07-26 22:33:34 -0500

    Add script that displays the number of rockets launches per rocket, sorted in descending order by number of launches and then in alphabetical order by rocket name

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -7,7 +7,23 @@ if rockets have the same amount of launches, in alphabetical order
 """
 
 
+import requests
+
+
 if __name__ == "__main__":
-    rocket = None
-    launches = None
-    print("{}: {}".format(rocket, launches))
+    url = 'https://api.spacexdata.com/v4/launches'
+    results = requests.get(url).json()
+    rocketDict = {}
+    for launch in results:
+        rocket = launch.get('rocket')
+        url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket)
+        results = requests.get(url).json()
+        rocket = results.get('name')
+        if rocketDict.get(rocket) is None:
+            rocketDict[rocket] = 1
+        else:
+            rocketDict[rocket] += 1
+    rocketList = sorted(rocketDict.items(), key=lambda kv: kv[0])
+    rocketList = sorted(rocketList, key=lambda kv: kv[1], reverse=True)
+    for rocket in rocketList:
+        print("{}: {}".format(rocket[0], rocket[1]))
