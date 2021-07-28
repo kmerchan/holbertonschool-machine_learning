3be2af63718ca00c1c0d9f97b07a3cca22c5f7e1
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 09:53:57 -0500

    Add script that computes average of all records from second_table in MySQL server and save in column with name 'average'

---
+++ pipeline/0x02-databases/5-average.sql
@@ -0,0 +1,4 @@
+-- Computes the average of all records in second_table in MySQL server
+-- resulting column name should be 'average'
+-- database name will be passed as an argument of the mysql command
+SELECT AVG(`score`) 'average' FROM `second_table`;
