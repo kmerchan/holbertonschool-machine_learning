1a8ca1cb30b5d150a0dd2ffa65b3e605b6247e68
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 19:55:19 -0500

    Add script that displays the average temperature by city ordered by descending temperature

---
+++ pipeline/0x02-databases/6-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Displays the average temperature by city ordered by temperature (desc)
+-- uses hbtn_0c_0 database
+-- database name will be passed as an argument of the mysql command
+SELECT city, AVG(value) AS average FROM temperatures GROUP BY city ORDER BY average DESC;
