233e5e04032558cf9a459b9eec487b1d032bd8c6
Kelsie Merchant <eislek02@gmail.com>
2021-07-29 02:50:37 +0000

    Update script to sum ratings and group by title; fix errors found during testing in sandbox

--- pipeline/0x02-databases/11-rating_shows.sql
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -1,9 +1,9 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating
 -- uses hbtn_0d_tvshows_rate database
 -- database name will be passed as an argument of the mysql command
-SELECT t.name AS tv_show, r.rate AS rating
+SELECT t.title AS title, SUM(r.rate) AS rating
        FROM tv_shows AS t
        LEFT JOIN tv_show_ratings AS r
        ON t.id = r.show_id
-       GROUP BY t.id
+       GROUP BY t.title
        ORDER BY rating DESC;
