01b448e862fc3ec1df42dc40ded213ae25bcefde
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 20:07:35 -0500

    Add script that displays the max temperature of each state ordered by state name

---
+++ pipeline/0x02-databases/7-max_state.sql
@@ -0,0 +1,4 @@
+-- Displays the max temperatures of each state, ordered by state name
+-- uses hbtn_0c_0 database
+-- database name will be passed as an argument of the mysql command
+SELECT `state`, MAX(value) AS maximum FROM temperatures GROUP BY `state` ORDER BY `state`;
