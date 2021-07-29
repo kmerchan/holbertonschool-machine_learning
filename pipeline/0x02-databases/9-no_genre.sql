4e43a7da68676652f4df541bd3fd2072050a6db1
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 20:42:51 -0500

    Add script that displays all shows in hbtn_0d_tvshows without a genre linked

---
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -0,0 +1,4 @@
+-- Lists all shows in hbtn_0d_tvshows without a genre listed
+-- uses hbtn_0d_tvshows database
+-- database name will be passed as an argument of the mysql command
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` WHERE g.`show_id` IS NULL ORDER BY s.`title` ASC;
