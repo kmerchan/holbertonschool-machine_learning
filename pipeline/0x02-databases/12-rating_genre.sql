d2216728dfb3843d37ddf2297bdc38b389318617
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 22:02:55 -0500

    Add script that lists all genres in hbtn_0d_tvshows_rate by their rating

---
+++ pipeline/0x02-databases/12-rating_genre.sql
@@ -0,0 +1,11 @@
+-- Lists all genres in the database by their rating
+-- uses hbtn_0d_tvshows_rate database
+-- database name will be passed as an argument of the mysql command
+SELECT g.name AS name, SUM(r.rate) AS rating
+       FROM tv_genres AS g
+       LEFT JOIN tv_show_genres as t
+       ON g.id = t.genre_id
+       LEFT JOIN tv_show_ratings AS r
+       ON t.show_id = r.show_id
+       GROUP BY g.name
+       ORDER BY rating DESC;
