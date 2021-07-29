0580556d10ccb4b3a9955eb650154bfcdd24c825
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 22:26:53 -0500

    Add script that creates a table called users with attributes that are primary key, never null, and/or unique

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Creates a table users with id, email, and name
+-- id: integer, never null, auto increment, and primary key
+-- email: string (255 chars), never null, unique
+-- name: string (255 chars)
+-- database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR (255)
+       );
