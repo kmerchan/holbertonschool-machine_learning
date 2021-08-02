4c4b4fa6e11b985729d308ec751c26ca3ce186da
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 13:11:43 -0500

    Update SQL keywords to be in uppercase to follow expected style guidelines

--- pipeline/0x02-databases/12-rating_genres.sql
+++ pipeline/0x02-databases/12-rating_genres.sql
@@ -3,7 +3,7 @@
 -- database name will be passed as an argument of the mysql command
 SELECT g.name AS name, SUM(r.rate) AS rating
        FROM tv_genres AS g
-       LEFT JOIN tv_show_genres as t
+       LEFT JOIN tv_show_genres AS t
        ON g.id = t.genre_id
        LEFT JOIN tv_show_ratings AS r
        ON t.show_id = r.show_id
