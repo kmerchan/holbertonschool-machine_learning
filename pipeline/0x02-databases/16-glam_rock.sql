99953998219d58dd69aa24264a797ceb8dbf4143
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 13:39:50 -0500

    Update script to use LIKE to compare string for band style

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -2,5 +2,5 @@
 -- utilizes the metal_bands table
 SELECT band_name, IF(split IS NULL, (2020 - formed), (split - formed)) AS lifespan
        FROM metal_bands
-       WHERE style CONTAINS 'Glam rock'
+       WHERE `style` LIKE '%Glam rock%'
        ORDER BY lifespan DESC;
