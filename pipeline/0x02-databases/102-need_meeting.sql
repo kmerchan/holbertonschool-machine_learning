c0e5f943f03a31cb74ec7ddae6b337118f3a42ea
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 23:58:27 -0500

    Add missing ; at end of statement

--- pipeline/0x02-databases/102-need_meeting.sql
+++ pipeline/0x02-databases/102-need_meeting.sql
@@ -1,7 +1,7 @@
 -- Creates a view need_meeting that lists all students that
 --    have a score less than 80 (strict)
 --    and no last_meeting or last_meeting more than a month
-DROP VIEW IF EXISTS need_meeting
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
        SELECT name
        FROM students
