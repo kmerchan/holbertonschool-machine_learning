018405a197b64781058aca42e3333750dc95d6f9
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 23:35:39 -0500

    Update script to use find, then count method

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -10,12 +10,12 @@ from pymongo import MongoClient
 if __name__ is "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
-    doc_count = logs.count_documents({})
+    doc_count = logs.find().count()
     print(doc_count, " logs")
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        method_count = logs.count_documents({"method": method})
+        method_count = logs.find({"method": method}).count()
         print("\tmethod ", method, ": ", method_count)
-    path_count = logs.count_documents({"method": "GET", "path": "/status"})
+    path_count = logs.find({"method": "GET", "path": "/status"}).count()
     print(path_count, " status check")
