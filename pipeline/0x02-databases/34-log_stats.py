98973656a9afe2864a96be7b938bb2164bad913e
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 23:41:49 -0500

    Add docstring under import guard

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -8,6 +8,9 @@ from pymongo import MongoClient
 
 
 if __name__ is "__main__":
+    """
+    Provides stats about Nginx logs stored in MongoDB
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_coll = client.logs.nginx
     doc_count = logs_coll.count_documents()
