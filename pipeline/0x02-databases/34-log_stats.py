0d8e774b2737f41ff567b7144ef205975ab961aa
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 23:39:54 -0500

    Update print statements to use formatted printing to try to see print output and change logs name to logs_coll

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -9,13 +9,14 @@ from pymongo import MongoClient
 
 if __name__ is "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    doc_count = logs.find().count()
-    print(doc_count, " logs")
+    logs_coll = client.logs.nginx
+    doc_count = logs_coll.count_documents()
+    print("{} logs".format(doc_count)
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        method_count = logs.find({"method": method}).count()
-        print("\tmethod ", method, ": ", method_count)
-    path_count = logs.find({"method": "GET", "path": "/status"}).count()
-    print(path_count, " status check")
+        method_count = logs_coll.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    path_count = logs_coll.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(path_count))
