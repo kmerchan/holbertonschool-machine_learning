5740f973f43944477b97bb6dbb78e5937fa214cd
Kelsie Merchant <eislek02@gmail.com>
2021-08-02 23:01:54 -0500

    Update function to use update_many instead of simply update to change all documents that match query

--- pipeline/0x02-databases/32-update_topics.py
+++ pipeline/0x02-databases/32-update_topics.py
@@ -16,5 +16,5 @@ def update_topics(mongo_collection, name, topics):
         topics [list of strings]:
             list of topics approached in the school
     """
-    mongo_collection.update({name: name},
-                            {'$set': {topics: topics}}, false, true)
+    mongo_collection.update_many({name: name},
+                                 {'$set': {topics: topics}})
