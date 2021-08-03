dda4dead21ca906e00dc5b5fa2035e1868e45576
Kelsie Merchant <eislek02@gmail.com>
2021-08-03 13:26:39 -0500

    Change name of procedure to remove Weighted to match expected procedure

--- pipeline/0x02-databases/103-average_weighted_score.sql
+++ pipeline/0x02-databases/103-average_weighted_score.sql
@@ -4,7 +4,7 @@
 --    user_id, a users.id value, can assume user_id is linked to existing users
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id_new INTEGER)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_new INTEGER)
 BEGIN
 	UPDATE users SET average_score=(
 	SELECT SUM(score * weight) / SUM(weight) FROM corrections
