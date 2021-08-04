659afbe64c13cd18179207cb2ab23325fff6d39f
Kelsie Merchant <eislek02@gmail.com>
2021-08-03 19:17:44 -0500

    Add print statements to test output

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -11,8 +11,10 @@ if __name__ is "__main__":
     """
     Provides stats about Nginx logs stored in MongoDB
     """
+    print("The program is running")
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_coll = client.logs.nginx
+    print("got collection")
     doc_count = logs_coll.count_documents({})
     print("{} logs".format(doc_count))
     print("Methods:")
