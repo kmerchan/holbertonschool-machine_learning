8eb2f32c97d81cfd3f5f2ed090edf650b69b2bdc
Kelsie Merchant <eislek02@gmail.com>
2021-08-03 19:28:24 -0500

    Remove docstring

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -8,9 +8,6 @@ from pymongo import MongoClient
 
 
 if __name__ is "__main__":
-    """
-    Provides stats about Nginx logs stored in MongoDB
-    """
     print("The program is running")
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_coll = client.logs.nginx
