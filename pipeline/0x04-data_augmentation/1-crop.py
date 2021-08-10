6c7e99c08026b19e1c6ad485d137cb7437777af4
Kelsie Merchant <eislek02@gmail.com>
2021-08-10 14:31:08 -0500

    Add function to crop an image to a given size

---
+++ pipeline/0x04-data_augmentation/1-crop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Defines function that performs a random crop of an image
+"""
+
+
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """
+    Performs a random crop of an image
+
+    parameters:
+        image [3D td.Tensor]:
+            contains the image to crop
+        size [tuple]:
+            contains the size of the crop
+
+    returns:
+        the cropped image
+    """
+    return (tf.image.crop_and_resize(image, crop_size=size))
