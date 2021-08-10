fb6f4143a0e37cf629c35b8b2d3c66a8c724b6da
Kelsie Merchant <eislek02@gmail.com>
2021-08-10 14:34:48 -0500

    Add function that rotates an image 90 degrees counter clockwise

---
+++ pipeline/0x04-data_augmentation/2-rotate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Defines function that rotates an image 90 degrees counter-clockwise
+"""
+
+
+import tensorflow as tf
+
+
+def rotate_image(image):
+    """
+    Rotates an image 90 degrees counter-clockwise
+
+    parameters:
+        image [3D td.Tensor]:
+            contains the image to crop
+
+    returns:
+        the rotated image
+    """
+    return (tf.image.rot90(image))
